cf2d496a08642a2eee9d194ac26bad4df999fb92
zhangcy11 <69498635+zhangcy11@users.noreply.github.com>
2020-08-31 20:51:06 +0800

    Update 存储查询.sql

--- 存储查询.sql
+++ 存储查询.sql
@@ -1,12 +1,3 @@
-create table student_stat as 
-select name,teacher,subject,score,avg_score,total_score,score/total_score as score_rate
-from student,subject,score,(select subject_id, avg(score) as avg_score from score group by subject_id) as t1,
-(select student_id,sum(score) as total_score from score group by student_id) as t2
-where student.id = score.student_id and subject.id = score.subject_id 
-and t1.subject_id = subject.id and t2.student_id = student.id;
-select * from student_stat;
-
-drop table student_stat;
 
 create procedure calc_student_stat()
 begin
